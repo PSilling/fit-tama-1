04ed2fddc00adbc91393fc7306bb3884c9ca3bd4
David Holas <mrkew48@gmail.com>
2022-10-15 04:57:08 +0200

    More helpers
    ...to ease the pain of the tragic Nullable implementation in this language

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -1,9 +1,34 @@
-
-extension NullSafety<T> on List<T> {
+extension ListNullSafety<T> on List<T> {
   T? elementAtOrNull(int index) {
     try {
       return elementAt(index);
     } on RangeError {
+      return null;
+    }
+  }
+}
+
+extension NullableListSafety<T> on List<T?> {
+  /// Maps elements of list to nullable, null elements after mapping are filtered
+  List<R> compactMap<R>(R? Function(T? element) mapper) {
+    var result = <R>[];
+    for (final element in this) {
+      final mapped = mapper(element);
+      if (mapped != null) {
+        result.add(mapped);
+      }
+    }
+    return result;
+  }
+}
+
+extension FunctionalNullable<T> on T? {
+  /// Transform value if not null, basically functor map from Haskell
+  R? flatMap<R>(R? Function(T value) mapper) {
+    final wrapped = this;
+    if (wrapped != null) {
+      return mapper(wrapped);
+    } else {
       return null;
     }
   }
