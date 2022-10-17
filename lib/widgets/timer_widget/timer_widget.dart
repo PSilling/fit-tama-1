bd2a6e41c0ef05dbccb01fd8087730b96338c533
MightyW0lf <wolferiuscze@gmail.com>
2022-10-17 14:31:27 +0200

    Implement play and pause timer widget behavior

--- lib/widgets/timer_widget/timer_widget.dart
+++ lib/widgets/timer_widget/timer_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tabletop_assistant/widgets/editable.dart';
 
@@ -12,6 +14,10 @@ class TimerWidget extends StatefulWidget {
   State<StatefulWidget> createState() => TimerWidgetState();
 }
 
+enum TimerWidgetTimerState {
+  init, running, paused
+}
+
 class TimerWidgetState extends State<TimerWidget>
     implements Editable<TimerWidget> {
   static const decoration = BoxDecoration(
@@ -20,8 +26,8 @@ class TimerWidgetState extends State<TimerWidget>
     boxShadow: [BoxShadow()],
   );
 
-  // TODO: Current time.
-  // TODO: Current state (init, running, paused).
+  late int _currentTime = _data.initialTime;
+  TimerWidgetTimerState _currentState = TimerWidgetTimerState.init;
   bool _isEditing = false;
   late TimerWidgetData _data;
 
@@ -36,21 +42,73 @@ class TimerWidgetState extends State<TimerWidget>
   }
 
   void start() {
-    // TODO: Start the timer.
-    throw UnimplementedError();
+    setState(() {
+      _currentState = TimerWidgetTimerState.running;
+    });
+    update();
   }
 
   void pause() {
-    // TODO: Pause the timer.
-    throw UnimplementedError();
+    setState(() {
+      _currentState = TimerWidgetTimerState.paused;
+    });
   }
 
   void reset() {
-    // TODO: Reset the timer to default value.
-    throw UnimplementedError();
+    setState(() {
+      _currentState = TimerWidgetTimerState.init;
+      _currentTime = _data.initialTime;
+    });
   }
 
-  // TODO: Method for updating each second when running?
+  void update() {
+    setState(() {
+      if (_currentState == TimerWidgetTimerState.running) {
+        _currentTime--;
+        Timer(const Duration(seconds: 1), update);
+      }
+    });
+  }
+
+  void _onTap() {
+    switch (_currentState) {
+      case TimerWidgetTimerState.init:
+        start();
+        break;
+      case TimerWidgetTimerState.running:
+        pause();
+        break;
+      case TimerWidgetTimerState.paused:
+        throw StateError("_onTap called when widget is paused.");
+    }
+  }
+
+  Widget _titleWidget(BuildContext context) {
+    final theme = Theme.of(context).textTheme;
+    return Text(_data.name, style: theme.headlineLarge);
+  }
+
+  Widget _timeWidget(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: Text( // TODO: Effect on value <= 0.
+                    "${_currentTime}s",
+                  ),
+                ),
+              )
+            ]
+        )
+    );
+  }
 
   // TODO: Editing dialog.
 
@@ -62,8 +120,27 @@ class TimerWidgetState extends State<TimerWidget>
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      decoration: decoration,
+      height: 240,
+      width: 240,
+      padding: const EdgeInsets.all(10),
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: _onTap,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [ // TODO: Add buttons (based on state) and remove _onTap.
+            _titleWidget(context),
+            Expanded(
+              child: _timeWidget(context),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
 }
