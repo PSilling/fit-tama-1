c83abdc24aa144263193f9ef342bda8c4de532d8
David Holas <mrkew48@gmail.com>
2022-11-07 21:45:48 +0100

    Dice widget number sizes made even

--- lib/widgets/dice_widget/dice_widget.dart
+++ lib/widgets/dice_widget/dice_widget.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
+import 'package:board_aid/helpers/measure_size.dart';
 import 'package:board_aid/themes.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../../helpers/extensions.dart';
@@ -17,14 +19,14 @@ class DiceWidget extends StatefulWidget {
   State<StatefulWidget> createState() => DiceWidgetState();
 }
 
-class DiceWidgetState extends State<DiceWidget>
-    implements Editable<DiceWidget> {
-
+class DiceWidgetState extends State<DiceWidget> implements Editable<DiceWidget> {
   final _random = Random();
 
   late DiceWidgetData _data;
   List<int>? _currentRoll;
   bool _isEditing = false;
+
+  final _resultHeight = ValueNotifier<double?>(null);
 
   @override
   bool get isEditing => _isEditing;
@@ -53,8 +55,7 @@ class DiceWidgetState extends State<DiceWidget>
     });
     await Future.delayed(const Duration(milliseconds: 100));
     setState(() {
-      _currentRoll = List.generate(
-          _data.numberOfDice, (_) => _randIntFrom1(to: _data.numberOfSides));
+      _currentRoll = List.generate(_data.numberOfDice, (_) => _randIntFrom1(to: _data.numberOfSides));
     });
   }
 
@@ -91,27 +92,36 @@ class DiceWidgetState extends State<DiceWidget>
             .reduce((value, element) => "$value+$element")
             .flatMap((value) => "$value=") ??
         "";
-    return FittedBox(
-      fit: BoxFit.contain,
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(minWidth: 1, minHeight: 1),
-        child: Text(
-          rollText,
-          style: ThemeHelper.widgetContentSecondary(context),
-        ),
+    return ValueListenableBuilder(
+      valueListenable: _resultHeight,
+      builder: (context, height, child) => SizedBox(
+        height: height.flatMap((value) => value*0.6),
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: Text(
+              rollText,
+              textAlign: TextAlign.right,
+              style: ThemeHelper.widgetContentSecondary(context),
+            ),
+          ),
+        //),
       ),
     );
   }
 
   Widget _resultText(BuildContext context) {
     final result = _currentRoll?.reduce((value, element) => value + element);
-    return FittedBox(
-      fit: BoxFit.contain,
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(minWidth: 1, minHeight: 1),
-        child: Text(
-          "${result ?? ""}",
-          style: ThemeHelper.widgetContentMain(context),
+    return MeasureSize(
+      onChange: (size) => _resultHeight.value = size.height,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(minWidth: 1, minHeight: 1),
+          child: Text(
+            "${result ?? ""}",
+            textAlign: TextAlign.left,
+            style: ThemeHelper.widgetContentMain(context),
+          ),
         ),
       ),
     );
@@ -129,17 +139,12 @@ class DiceWidgetState extends State<DiceWidget>
           children: [
             if (_data.numberOfDice > 1)
               Expanded(
-                flex: 2,
-                child: FractionallySizedBox(
-                  heightFactor: 0.5,
-                  child: Opacity(
-                    opacity: 0.4,
-                    child: _diceText(context),
-                  ),
+                child: Opacity(
+                  opacity: 0.4,
+                  child: _diceText(context),
                 ),
               ),
             Expanded(
-              flex: 1,
               child: _resultText(context),
             )
           ]));
