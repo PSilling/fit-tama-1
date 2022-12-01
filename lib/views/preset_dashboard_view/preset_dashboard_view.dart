45a5ac5bff3e973f6f07a1b406ba48303dd10e30
xcoupe01 <vojtech.coupek@email.cz>
2022-12-01 16:21:52 +0100

    dashboard title edit simplification

--- lib/views/preset_dashboard_view/preset_dashboard_view.dart
+++ lib/views/preset_dashboard_view/preset_dashboard_view.dart
@@ -41,7 +41,6 @@ class _PresetDashboardViewState extends State<PresetDashboardView> {
   bool editVisible = false;
   late FocusNode textFocusNode;
   late String nameEdit;
-  bool titleEdit = false;
 
   @override
   void initState() {
@@ -81,44 +80,24 @@ class _PresetDashboardViewState extends State<PresetDashboardView> {
         appBar: AppBar(
           title: Stack(
             children: [
-              Visibility(
-                visible: !titleEdit,
-                child: GestureDetector(
-                  child: Text(
-                    widget.preset.name,
-                    style: const TextStyle(
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  onTap: (){
-                    titleEdit = true;
-                    setState(() {});
-                    textFocusNode.requestFocus();
-                  },
+              TextFormField(
+                focusNode: textFocusNode,
+                style: Theme.of(context).textTheme.titleLarge,
+                cursorColor: ThemeHelper.dialogForeground(context),
+                decoration: ThemeHelper.dialogInputDecoration(
+                  context,
+                  hasBorder: false,
+                  hasPadding: false
                 ),
-              ),
-              Visibility(
-                visible: titleEdit,
-                child: TextFormField(
-                  focusNode: textFocusNode,
-                  style: Theme.of(context).textTheme.titleLarge,
-                  cursorColor: ThemeHelper.dialogForeground(context),
-                  decoration: ThemeHelper.dialogInputDecoration(
-                    context,
-                    hasBorder: false,
-                    hasPadding: false
-                  ),
-                  initialValue: widget.preset.name,
-                  textInputAction: TextInputAction.done,
-                  onTap: () => textFocusNode.requestFocus(),
-                  onChanged: (value) {nameEdit = value;},
-                  onEditingComplete: () {
-                    widget.preset.name = nameEdit;
-                    textFocusNode.unfocus();
-                    titleEdit = false;
-                    setState(() {});
-                  },
-                ),
+                initialValue: widget.preset.name,
+                textInputAction: TextInputAction.done,
+                onTap: () => textFocusNode.requestFocus(),
+                onChanged: (value) {nameEdit = value;},
+                onEditingComplete: () {
+                  widget.preset.name = nameEdit;
+                  textFocusNode.unfocus();
+                  setState(() {});
+                },
               ),
             ]
           ),
